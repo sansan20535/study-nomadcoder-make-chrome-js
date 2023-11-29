56225437048160081196b2ee75e19eb0eb88cef2
sansan20535 <20san20@daum.net>
2023-11-29 23:34:14 +0900

    Feat : #3.3 Events - add addEventListener - click title to log message

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const title = document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
+function handleTitleClick(){
+    console.log("title was clicked!");
+}
 
-title.style.color = "blue";
+title.addEventListener("click", handleTitleClick);
