dd58d0d290d685297166993f8b4a0c055ddaa508
sansan20535 <20san20@daum.net>
2023-12-26 18:04:10 +0900

    [Feat] #3.4 Events part Two

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    console.log("title was clicked!");
+    title.style.color = "blue"; 
+}
+
+function handleMouseEnter(){
+    title.innerHTML = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerHTML = "Mouse is gone!";
 }
 
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
